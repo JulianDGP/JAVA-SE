3f7462f75c5f521d8cd9325168f0800b091d781f
JulianDGP <juliandgp@hotmail.com>
2022-10-28 16:11:44 -0500

    Se cambia

--- HolaMundo/src/EjemploClaseMath.java
+++ HolaMundo/src/EjemploClaseMath.java
@@ -1,5 +1,7 @@
 public class EjemploClaseMath {
     public static void main(String[] args) {
         System.out.println("git push example ");
+        System.out.println("probando el bash x2");
+        System.out.println("probando el bash x3");
     }
 }
