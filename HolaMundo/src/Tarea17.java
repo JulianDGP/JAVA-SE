c967979fe2d3916aa31a37ddba5840ff69cd0f4f
JulianDGP <juliandgp@hotmail.com>
2022-12-21 18:13:25 -0500

    cambios prueba

--- HolaMundo/src/Tarea17.java
+++ HolaMundo/src/Tarea17.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Tarea17 {
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int valores[] = new int[12];
+        int histograma[] = new int[7];
+
+        for (int i=0; i<valores.length; i++){
+            valores[i] = in.nextInt();
+            histograma[valores[i]] +=1;
+
+        }
+        String asterisco = "*";
+        for ( int i =1; i<histograma.length; i++){
+            System.out.println(i+": " + asterisco.repeat(histograma[i]));
+        }
 
     }
 }
