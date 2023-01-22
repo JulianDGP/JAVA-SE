7f6c6c648770fe2f389512c66730653383900fd7
JulianDGP <juliandgp@hotmail.com>
2023-01-22 11:12:14 -0500

    Cambios

--- POO/src/RelacionesObjetos.java
+++ POO/src/RelacionesObjetos.java
@@ -1,24 +1,31 @@
 public class RelacionesObjetos {
     public static void main(String[] args) {
+        Persona conductorSubaru = new Persona("Luci", "Martinez");
+
         Automovil subaru = new Automovil("Subaru", "Impreza");
 
         Motor motorSubaru = new Motor(2.0, TipoMotor.BENCINA);
         Estanque estanqueSubaru = new Estanque(50);
         subaru.setMotor(motorSubaru);
         subaru.setEstanque(estanqueSubaru);
+        subaru.setColor(Color.BLANCO);
+        subaru.setTipo(TipoAutomovil.HATCHBACK);
+        subaru.setConductor(conductorSubaru);
 
-        Automovil mazda = new Automovil("Mazda", "BT-50", Color.GRIS,motorSubaru);
+        Persona pato = new Persona("Pato","Rodriguez");
+        Automovil mazda = new Automovil("Mazda", "BT-50", Color.GRIS, new Motor(3.0, TipoMotor.DIESEL));
+        mazda.setTipo(TipoAutomovil.PICKUP);
 
+
+
+        Persona bea = new Persona("Bea","Gonzalez");
         Automovil nissan = new Automovil("Nissan","Navara", Color.GRIS, motorSubaru, new Estanque(50));
 
-
-        System.out.println(mazda.acelerarFrenar((400)));
 
         System.out.println("kilomentros por litro"+ subaru.calcularConsumo(300,0.6f));
         System.out.println("kilomentros por litro"+ subaru.calcularConsumo(300,60));
 
         subaru.setTipo(TipoAutomovil.HATCHBACK);
-        mazda.setTipo(TipoAutomovil.PICKUP);
 
         nissan.setTipo(TipoAutomovil.CONVERTIBLE);
         System.out.println(nissan);
